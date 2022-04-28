80cc456b8d43ef5f237c8fb7be5ba57a2118d348
김서지 <seojikim@gimseojiui-MacBookPro.local>
2022-04-28 19:35:04 +0900

    Modify directory name to fix error
    Signed-off-by: 김서지 <seojikim@gimseojiui-MacBookPro.local>

--- package-install.sh
+++ package-install.sh
@@ -10,6 +10,8 @@ cd /srv/releases.nexell.co.kr
 #git clone http://git.nexell.co.kr:8081/gerrit/nexell/infrastructure/linaro-license-protection-configs.git configs
 git clone https://github.com/seojineer/infrastructure_server_linaro-license-protection.git
 git clone https://github.com/seojineer/infrastructure_server_linaro-license-protection-configs.git
+mv infrastructure_server_linaro-license-protection linaro-license-protection
+mv infrastructure_server_linaro-license-protection-configs configs
 cd linaro-license-protection
 pip install -r requirements.txt
 
