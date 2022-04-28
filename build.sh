2d43f230496ced3e274138a8478ca0834d31a8c0
김서지 <seojikim@gimseojiui-MacBookPro.local>
2022-04-28 19:34:10 +0900

    Modify dockerhub repository as seojikim in build script
    Signed-off-by: 김서지 <seojikim@gimseojiui-MacBookPro.local>

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 docker pull ubuntu:16.04
-docker build -t nexell/fileserver-base .
+docker build -t seojikim/fileserver-base .
 
 cd deploy_releases
-docker build -t nexell/fileserver-releases .
+docker build -t seojikim/fileserver-releases .
 
-cd deploy_snapshot
-docker build -t nexell/fileserver-snapshot .
+cd ../deploy_snapshot
+docker build -t seojikim/fileserver-snapshot .
